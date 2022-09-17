d168063febff27724440aa532a9ff6a17bcceb29
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-17 09:27:37 +0300

    Task 5_38 completed

--- hw5_38/Program.cs
+++ hw5_38/Program.cs
@@ -1,14 +1,33 @@
 ﻿/*
-Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементами массива.
 [3 7 22 2 78] -> 76
 */
 
 double GetDiff(double[] array)
 {
+    double minElem = array[0];
+    double maxElem = array[0];
+    double diff = 0;
 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < minElem)
+        {
+            minElem = array[i];
+        }
+
+        if (array[i] > maxElem)
+        {
+            maxElem = array[i];
+        }
+    }
+
+    diff = Math.Round(maxElem - minElem, 2);
+    return diff;
 }
 
-double[] array = new double[] {2.5, 3.2, 9.9, -4.7};
-double differece = GetDiff(array);
+double[] array = new double[] {2.5, 3.2, 9.93, -4.7};
+double difference = GetDiff(array);
 
-Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {difference}");
